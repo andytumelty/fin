2cb046c831d8a2e1e162206e0469d2345ea4472e
Andrew Tumelty <andrew.tumelty@gmail.com>
2014-01-05 00:18:32 +0000

    remove transaction validation on amount numericality if nil

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,7 +3,8 @@ class Transaction < ActiveRecord::Base
   belongs_to :category
   has_one :user, :through => :account
   validates :description, presence: true
-  validates :amount, presence: true, numericality: true
+  validates :amount, presence: true
+  validates :amount, numericality: true, if: "!amount.nil?"
   validates :account, presence: true
   validates :category, presence: true
 end
