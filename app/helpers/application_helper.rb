5b8f36d08c64ab96f050f641aa296f85d490a705
Andrew Tumelty <andrew.tumelty@gmail.com>
2014-01-18 21:15:40 +0000

    number_to_currency_td helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def number_to_currency_td(number)
+    number_to_currency(number,format: "<td align=\"right\">%u</td><td align=\"right\">%n</td>", negative_format: "<td align=\"right\">- %u</td><td align=\"right\">%n</td>")
+  end
 end
