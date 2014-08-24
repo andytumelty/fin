d697532514bff56aaea287db4f70e254e26afbf9
Andrew Tumelty <andrew.tumelty@gmail.com>
2014-08-24 17:45:19 +0100

    Strip custom callbacks in prep for rewrite

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,11 +3,11 @@ class Account < ActiveRecord::Base
 	has_many :transactions, dependent: :destroy
   validates :name, presence: true, uniqueness: { scope: :user, case_sensitive: false }
 
-  after_destroy :trigger_balance_calcs
+  #after_destroy :trigger_balance_calcs
 
-  def trigger_balance_calcs
-    puts "%%%%% account.rb : trigger_balance_calcs"
-    self.user.transactions.order(date: :asc, order: :asc).first.update_transaction_balances
-  end
+  #def trigger_balance_calcs
+  #  puts "%%%%% account.rb : trigger_balance_calcs"
+  #  self.user.transactions.order(date: :asc, order: :asc).first.update_transaction_balances
+  #end
 
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -5,14 +5,14 @@ class Category < ActiveRecord::Base
 
   validates :name, presence: true, uniqueness: { scope: :user, case_sensitive: false }
 
-  after_destroy :reassign_transactions, :delete_reservations
+  after_destroy :reassign_transactions#, :delete_reservations
 
   def reassign_transactions
     transactions = self.user.transactions.where(category: self)
     transactions.update_all(category: self.user.categories.where(name: "unassigned").first)
   end
 
-  def delete_reservations
-    reservations.where(category_id: self.id).delete
-  end
+  #def delete_reservations
+  #  reservations.where(category_id: self.id).delete
+  #end
 end
