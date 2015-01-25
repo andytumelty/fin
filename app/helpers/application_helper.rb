4574808a9eb3c144af17b4c6f46f6fd9bb6fa07e
Andrew Tumelty <andrew.tumelty@gmail.com>
2015-01-25 22:01:56 +0000

    DRY up number_to_currency_td so it doesn't suck as much

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,14 @@
 module ApplicationHelper
   def number_to_currency_td(number, class_label)
+    sign = number < 0 ? '-' : ''
     number_to_currency(
       number,
-      format: ("<td align=\"right\"><span class=\"" + class_label + "_sign\"></span><span class=\"" + class_label + "_currency\">%u</span></td><td class=\"" + class_label + "_amount\" align=\"right\">%n</td>").html_safe, 
-      negative_format: ("<td align=\"right\"><span class=\"" + class_label + "_sign\">-</span><span class=\"" + class_label + "_currency\">%u</span><td class=\"" + class_label + "_amount\" align=\"right\">%n</td>").html_safe )
+      format: (
+        '<td align="right">' +\
+          '<span class="' + class_label + '_sign">' + sign + '</span>' +\
+          '<span class="' + class_label + '_currency">%u</span>' +\
+        '</td>' +\
+        '<td class="' + class_label + '_amount" align="right">%n</td>'
+      ).html_safe)
   end
 end
