eecd0c98e861e70623e929b60e5318884f89819f
Andrew Tumelty <andrew.tumelty@gmail.com>
2015-04-05 16:18:41 +0100

    Remote Accounts: Add some validation to the model

--- app/models/remote_account.rb
+++ app/models/remote_account.rb
@@ -1,4 +1,11 @@
 class RemoteAccount < ActiveRecord::Base
   belongs_to :account
   belongs_to :remote_account_type
+
+  validates :title, presence: true
+  validates :user_credential, presence: true
+  validates :remote_account_identifier, presence: true
+  validates :account_id, presence: true
+  validates :remote_account_type_id, presence: true
+  validates :sync_from, presence: true
 end
