3f008d4c8f0a10530b971f80157d42a2bc488b4d
Andrew Tumelty <andrew.tumelty@gmail.com>
2015-12-22 18:03:47 +0000

    Tidy up transaction_with_balance model and ensure readonly

--- app/models/transaction_with_balance.rb
+++ app/models/transaction_with_balance.rb
@@ -1,7 +1,5 @@
 class TransactionWithBalance < ActiveRecord::Base
-  #belongs_to :transaction
+  after_initialize :readonly!
+
   belongs_to :parent_transaction, foreign_key: "transaction_id", class_name: "Transaction"
-  #has_one :category, :through => :transaction
-  #has_one :account, :through => :transaction
-  #has_one :user, :through => :transaction
 end
