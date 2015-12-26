2e0ce317805ae482efeb32fe62af15635bac8a02
Andrew Tumelty <andrew.tumelty@gmail.com>
2015-12-26 14:21:13 +0000

    Fix fixtures and a bit of a test related cleanup

--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -4,4 +4,9 @@ class AccountTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not save without name" do
+    account = Account.new(user: users(:user_1))
+    assert_not account.save
+  end
 end
