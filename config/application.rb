8a69e79d24ec128fb85fd4a1a81ec14b7919f3a8
Andrew Tumelty <andrew.tumelty@gmail.com>
2016-01-03 23:24:25 +0000

    Remove bourbon, bitters and neat

--- config/application.rb
+++ config/application.rb
@@ -22,8 +22,5 @@ module Fin
     # config.i18n.default_locale = :de
     config.autoload_paths << Rails.root.join('lib')
     config.active_record.schema_format = :sql
-
-    config.assets.compile = true
-    config.assets.precompile =  ['*.js', '*.css', '*.css.erb'] 
   end
 end
