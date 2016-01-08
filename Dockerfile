469c1ef458b3f6a77ce4b337d2593fd18851298b
Andrew Tumelty <andrew.tumelty@gmail.com>
2016-01-08 20:46:32 +0000

    FIN-22 Fix Amex sync setup and logic for docker

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,14 @@
 FROM ruby:2.2.0
-RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs
+RUN apt-get -qqy update && \
+    apt-get -qqy install build-essential libpq-dev nodejs
 # rails runs pg_dump on db:migrate, so need this installed
 # see https://github.com/docker-library/rails/issues/13
-RUN apt-get update && apt-get install -y postgresql-client --no-install-recommends && rm -rf /var/lib/apt/lists/*
+RUN apt-get -qqy update && \
+    apt-get -qqy install postgresql-client --no-install-recommends
+# for amex headless
+RUN apt-get -qqy update && \
+    apt-get -qqy install xvfb iceweasel
+RUN rm -rf /var/lib/apt/lists/*
 RUN mkdir /fin
 WORKDIR /fin
 ADD Gemfile /fin/Gemfile
--- lib/amex.rb
+++ lib/amex.rb
@@ -22,9 +22,16 @@ class Amex
 
   def transactions(start_date, end_date, account)
     @ua.link(text: "Your Statement").click
-    @ua.link(id: "date-layer-link").click
+    @ua.link(text: "Your Card Activity").click
+    @ua.link(id: "date-layer-link").parent.click
     @ua.link(text: "Date range").click
     #puts "from: #{start_date}"
+    wait_count = 0
+    while ! @ua.div(id: "from-datepicker").visible? && wait_count < 5
+      #puts "Can't find datepicker yet, sleeping"
+      sleep 1
+      wait_count += 1
+    end
     @ua.div(id: "from-datepicker").select_list(class: "ui-datepicker-year").select(Date.parse(start_date).strftime("%Y"))
     @ua.div(id: "from-datepicker").select_list(class: "ui-datepicker-month").select_value(Date.parse(start_date).strftime("%-m").to_i - 1)
     @ua.div(id: "from-datepicker").link(text: Date.parse(start_date).strftime("%-d")).click
