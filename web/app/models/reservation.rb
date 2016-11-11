eb64404e124febce5145a59b5421c29a4297849c
Andrew Tumelty <andrew.tumelty@gmail.com>
2016-11-11 09:24:53 +0000

    Add transactions method to budget and reservations

--- web/app/models/reservation.rb
+++ web/app/models/reservation.rb
@@ -10,4 +10,15 @@ class Reservation < ActiveRecord::Base
   def balance
     return self.related_transactions.sum("amount")
   end
+
+  def transactions
+    budget_transactions = self.user.transactions.where(budget_date: (self.budget.start_date..self.budget.end_date))
+    # TODO make category_id for everything_else reservation 0 instead of nil
+    if self.category_id.nil?
+      transactions = budget_transactions.where.not(category_id: self.budget.reservations.map { |r| r.category_id })
+    else
+      transactions = budget_transactions.where(category_id: self.category_id)
+    end
+    return transactions
+  end
 end
